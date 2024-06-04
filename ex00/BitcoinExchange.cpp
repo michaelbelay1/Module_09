cfe2217a68424035d6d3eb4c7860289184e956c1
Michael Haile <mhaile@lab3r6s1.42abudhabi.ae>
2024-06-04 22:45:08 +0400

    Updated on 2024-06-04 22:45:08 by mhaile

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,34 +6,14 @@
 /*   By: mhaile <mhaile@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:59:10 by mhaile            #+#    #+#             */
-/*   Updated: 2024/06/04 16:24:13 by mhaile           ###   ########.fr       */
+/*   Updated: 2024/06/04 22:43:58 by mhaile           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-
 BitcoinExchange::BitcoinExchange() {
-	std::ifstream file("data.csv");
-	std::string line;
 	
-	if (file.is_open()) {
-		if (file.peek() == EOF)
-			throw invalidDBException();
-	}
-	if (file.is_open()) {
-		std::getline(file, line);
-		if (line.length() != 18 || line != "date,exchange_rate")
-			throw invalidDBException();
-		while(std::getline(file, line)) {
-			std::string key = line.substr(0, line.find(","));
-			std::string value = line.substr(line.find(",") + 1, line.length());
-			_data[key] = value;
-		}
-		file.close();
-	}
-	else
-		throw fileNotFoundException();
 }
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& src) {
@@ -55,20 +35,25 @@ void BitcoinExchange::printData() {
     }
 }
 
+// Getter for _data.
+std::map<std::string, std::string> BitcoinExchange::getData() {
+	return _data;
+}
+
+// returns true if infile is open and not empty, else throws exception
 bool BitcoinExchange::is_infile(std::ifstream& file) {
 	if (file.is_open()) {
 		if (file.peek() == EOF) {
-			std::cout << "input file is empty" << std::endl;
-			return false;
+			throw fileNotFoundException();
 		}
 	}
 	else {
-		std::cout << "File not found" << std::endl;
-		return false;
+		throw fileNotFoundException();
 	}
 	return true;
 }
 
+// returns true if string is a digit, else false
 bool BitcoinExchange::isDigit(std::string str) {
 	for(int i = 0; i < (int)str.length(); i++) {
 		if (!isdigit(str[i])) {
@@ -79,17 +64,16 @@ bool BitcoinExchange::isDigit(std::string str) {
 	return true;
 }
 
+// Checks if a year is a leap year. Returns false if it is not.
 bool BitcoinExchange::isLeapYear(int dayInt, int monthInt, int yearInt) {
 	if (monthInt == 2) {
 		if ((yearInt % 4 == 0 && yearInt % 100 != 0) || (yearInt % 400 == 0)) {
 			if (dayInt > 29) {
-				// std::cout << "Invalid date" << std::endl;
 				return false;
 			}
 		}
 		else {
 			if (dayInt > 28) {
-				// std::cout << "Invalid date" << std::endl;
 				return false;
 			}
 		}
@@ -97,6 +81,7 @@ bool BitcoinExchange::isLeapYear(int dayInt, int monthInt, int yearInt) {
 	return true;
 }
 
+// Parse value from the input file. Returns 1 if value is invalid.
 int BitcoinExchange::parseValue(std::string value) {
 	if (value.empty()) {
 		std::cout << "Error: Value is empty" << std::endl;
@@ -105,7 +90,7 @@ int BitcoinExchange::parseValue(std::string value) {
 	char *endptr;
 	double val = std::strtod(value.c_str(), &endptr);
 	if (*endptr != '\0') {
-		std::cout << "Error: Invalid value: " << value << std::endl;
+		std::cout << "Error: bad input => " << value << std::endl;
 		return 1;
 	}
 	if (val < 0) {
@@ -117,15 +102,10 @@ int BitcoinExchange::parseValue(std::string value) {
 		return 1;
 	}
 
-	// for (int i = 1; i < (int)value.length() - 1; i++) {
-	// 	if (!isdigit(value[i]) && value[i] != '.' && value[i] != '-' && value[i] != '+') {
-	// 		std::cout << "Error: Nondigit character found on Value" << value[i] << std::endl;
-	// 		return 1;
-	// 	}
-	// }
 	return 0;
 }
 
+// Trim whitespace from a string.
 std::string BitcoinExchange::ft_trim(std::string str) {
 	size_t first = str.find_first_not_of(' ');
     if (std::string::npos == first)
@@ -135,54 +115,97 @@ std::string BitcoinExchange::ft_trim(std::string str) {
     return str.substr(first, (last - first + 1));
 }
 
+// Get the closest previous day from a given dates. 
 std::string BitcoinExchange::previousDay(std::string& date) {
-	int year = ft_stoi(date.substr(0, 4));
-	int month = ft_stoi(date.substr(5, 2));
-	int day = ft_stoi(date.substr(8, 2));
-	
-	// daysInMonth = number of days in each month
-	int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 0 is a placeholder
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 			// check if year is a leap year
-		daysInMonth[2] = 29;
-	day--;
-	if (day == 0) {
-		month--;
-		if (month == 0) {
-			year--;
-			month = 12;
-		}
-		day = daysInMonth[month];
+	std::map<std::string, std::string>::iterator it;
+	std::map<std::string, std::string>  mydata = getData();
+
+	it = mydata.lower_bound(date);
+	if (it == mydata.end()) {
+		std::cout << "Error: No data found for " << date << std::endl;
+		return "2009-01-01";
 	}
-	std::string newYear = ft_toStr(year) + "-";
-	std::string newMonth = (month < 10) ? "0" + ft_toStr(month) + "-": ft_toStr(month) + "-";
-	std::string newDay = (month < 10) ? "0" + ft_toStr(day) : ft_toStr(day);
-	return newYear + newMonth + newDay;
+	return (it->first);
 }
 
-std::string BitcoinExchange::ft_toStr(int num) {
-	std::stringstream ss;
-	ss << num;
-	return ss.str();
-}
-
+// Convert a string to an integer using stringstream.
 double BitcoinExchange::ft_stoi(const std::string& str) {
-	std::stringstream ss(str);
 	double num;
+	std::stringstream ss(str);
 	ss >> num;
 	return num;
 }
 
-int BitcoinExchange::parseData(std::ifstream& file) {
+// Parse date from the input and DB files. Returns 1 if date is invalid.
+int BitcoinExchange::parseDate(std::string date) {
+	std::string year = date.substr(0, 4);
+	std::string month = date.substr(5, 2);
+	std::string day = date.substr(8, 2);
+	int yearInt, monthInt, dayInt;
+
+	if (date.length() != 10 || date[4] != '-' || date[7] != '-'
+		|| !isDigit(year) || !isDigit(month) || !isDigit(day))
+		return 1;
+	
+	std::stringstream ssYear(year);
+	std::stringstream ssMonth(month);
+	std::stringstream ssDay(day);
+
+	ssYear >> yearInt;
+	ssMonth >> monthInt;
+	ssDay >> dayInt;
+
+	if ((yearInt < 2009 || yearInt > 2022)
+		|| (monthInt < 1 || monthInt > 12)
+		|| (dayInt < 1 || dayInt > 31)) {
+		return 1;
+	}
+	if (yearInt == 2009) {
+		if (monthInt == 1) {
+			if (dayInt < 2) {
+				return 1;
+			}
+		}
+	}
+
+	if ((monthInt == 4 || monthInt == 6 || monthInt == 9
+		|| monthInt == 11) && dayInt > 30) {
+		return 1;
+	}
+	
+	if (!isLeapYear(dayInt, monthInt, yearInt))
+		return 1;
+		
+	return 0;
+}
+
+// Find the lower closest date to the input date. 
+std::map<std::string, std::string>::iterator  BitcoinExchange::closestDate(std::string date) {
+	std::map<std::string, std::string>::iterator it;
+	std::string prevDate = date;
+	it = _data.find(date);
+	while (it == _data.end()) {
+		std::string& currentDate = prevDate;
+		prevDate = previousDay(currentDate);
+		if (prevDate == "2009-01-01")
+			break;
+		it = _data.find(prevDate);
+	}
+	return it;
+}
+
+// After parsing is done, extract the value from the input file. Display the final result.
+int BitcoinExchange::extractValue(std::ifstream& file) {
 	std::string line;
 
 	std::getline(file, line);
-	for (int i = 0; i < (int)line.length() - 1; i++) {
-		if (line[i] != "date | value"[i]) {
-			std::cout << "Invalid infile header formate" << std::endl;
-			return 1;
-		}
+	if (line[line.length() - 1] == '\r')
+		line = line.substr(0, line.length() - 1);
+	if (line != "date | value") {
+		std::cout << "Invalid infile header formate" << std::endl;
+		return 1;
 	}
-	
+		
 	while (std::getline(file, line)) {
 		size_t pos = line.find('|');
 		std::string date = ft_trim(line.substr(0, pos));
@@ -190,87 +213,71 @@ int BitcoinExchange::parseData(std::ifstream& file) {
 		if (value[value.length() - 1] == '\r')
 			value = value.substr(0, value.length() - 1);
 	
-		// std::cout << value << std::endl;
-		if (date.length() != 10) {
-			std::cout << "Error: bad input => " << date << std::endl;
-			continue;
-		}
-		if (date[4] != '-' || date[7] != '-') {
-			std::cout << "Error: bad input => " << date << std::endl;
-        	continue;
-		}
-
-		std::string year = date.substr(0, 4);
-		std::string month = date.substr(5, 2);
-		std::string day = date.substr(8, 2);
-
-		if (!isDigit(year) || !isDigit(month) || !isDigit(day))
-			continue;
-
 		if (parseValue(value))
 			continue;
-
-		int yearInt, monthInt, dayInt;
-	
-		std::stringstream ssYear(year);
-		std::stringstream ssMonth(month);
-		std::stringstream ssDay(day);
-
-		ssYear >> yearInt;
-		ssMonth >> monthInt;
-		ssDay >> dayInt;
-
-		if ((yearInt < 2009 || yearInt > 2022)
-			|| (monthInt < 1 || monthInt > 12)
-			|| (dayInt < 1 || dayInt > 31)) {
-			std::cout << "Error: bad input => " << date << std::endl;
-			continue;
-		}
-		if (yearInt == 2009) {
-			if (monthInt == 1) {
-				if (dayInt < 2) {
-					std::cout << "Error: bad input => " << date << std::endl;
-					continue;
-				}
-			}
-		}
-
-		if ((monthInt == 4 || monthInt == 6 || monthInt == 9
-			|| monthInt == 11) && dayInt > 30) {
+		
+		if (parseDate(date)) {
 			std::cout << "Error: bad input => " << date << std::endl;
 			continue;
 		}
 
-		if (!isLeapYear(dayInt, monthInt, yearInt)) {
-			std::cout << "Error: bad input => " << date << std::endl;
-			continue;
-		}
-
-		std::map<std::string, std::string>::iterator it;
-		std::string prevDate = date;
-		it = _data.find(date);
-		while (it == _data.end()) {
-			std::string& currentDate = prevDate;
-			prevDate = previousDay(currentDate);
-			if (prevDate == "2009-01-01") {
-				std::cout << "Error: No data found for " << date << std::endl;
-				break;
-			}
-			it = _data.find(prevDate);
-		}
+		std::map<std::string, std::string>::iterator it = closestDate(date);
+		
 		std::cout << date << " => " << value << " = " << ft_stoi(it->second) * ft_stoi(value) << std::endl;
 	}
 	return 0;
 }
 
-void BitcoinExchange::exec(std::string input) {
-	std::ifstream file(input.c_str());
+// Parse DB, then store the data in a map. returns 1 if DB is invalid.
+int BitcoinExchange::setDB(std::ifstream& file) {
 	std::string line;
+
+	if (!file.is_open())
+		throw fileNotFoundException();
+
+	if (file.peek() == EOF)
+		throw invalidDBException();
+	
+	std::getline(file, line);
+	if (line.length() != 18 || line != "date,exchange_rate")
+		throw invalidDBException();
+	while(std::getline(file, line)) {
+		std::string date = ft_trim(line.substr(0, line.find(",")));
+		std::string value = ft_trim(line.substr(line.find(",") + 1, line.length()));
+		
+		if (date.length() != 10 || (date[4] != '-' || date[7] != '-')) {
+			std::cout << "Error: date formate in DB => " << date << std::endl;
+			return (1);
+		}
+
+		char *endptr;
+		double val = std::strtod(value.c_str(), &endptr);
+		if (*endptr != '\0' || val < 0) {
+			std::cout << "Error: Invalid value in DB: " << value << std::endl;
+			return (1);
+		}
+		
+		if (parseDate(date)) {
+			std::cout << "Error: Invalid date in DB: " << date << std::endl;
+			return (1);
+		}
+	
+		_data[date] = value;
+	}
+	return (0);
+}
+
+// Main function to execute the program.
+void BitcoinExchange::exec(std::string input) {
+	std::ifstream DB("data.csv");
+	std::ifstream file(input.c_str());
+
+	if (setDB(DB))
+		return ;
+
 	if (!this->is_infile(file))
 		return ;
 
-	if (this->parseData(file))
+	if (this->extractValue(file))
 		return ;
-	
-	// printf("File found and has something inside\n");
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mhaile <mhaile@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:36:56 by mhaile            #+#    #+#             */
-/*   Updated: 2024/06/04 16:18:33 by mhaile           ###   ########.fr       */
+/*   Updated: 2024/06/04 22:36:33 by mhaile           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,14 @@ class BitcoinExchange {
 		
 		bool is_infile(std::ifstream& file); 	// Parse input file and check if it is empty.
 		bool isDigit(std::string str); 			// Check if a string is a digit.
-		int  parseData(std::ifstream& file);	// Parse data inside the input file.
-		int  parseValue(std::string value); 	// Parse value from the input file.
+		int  setDB(std::ifstream& file); 		// Set the database from the input file.
+		int  extractValue(std::ifstream& file);	// Parse data inside the input file.
+		int  parseDate(std::string date); 		// Parse date from the input file.
+		int  parseValue(std::string date); 		// Parse value from the input file.
+		std::map<std::string, std::string>::iterator closestDate(std::string date); 	// Find the lower closest date to the input date.
 		std::string ft_trim(std::string str); 	// Trim whitespace from a string.
 		bool isLeapYear(int dayInt, int monthInt, int yearInt); // Check if a year is a leap year.
 		std::string previousDay(std::string& date); 			// Get the previous day from a given date.
-		std::string ft_toStr(int num);			// Convert an integer to a string.
 		double ft_stoi(const std::string& str); // Convert a string to an integer.
 
 	public:
@@ -45,7 +47,8 @@ class BitcoinExchange {
 		~BitcoinExchange();
 		
 		void exec(std::string input); 			// Main function to execute the program.
-		void printData();       				// Function to print map data. To be deleted later.
+		void printData(); 				// Function to print map data. To be deleted later.
+		std::map<std::string, std::string> getData(); // Getter for _data.
 		// *** Exception classes *** //
 		class fileNotFoundException : public std::exception {
 			public:
