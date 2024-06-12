9c2440fdae91e4664d9bddb4eec113b985a9f7f9
Michael Haile <mhaile@lab1r4s7.42abudhabi.ae>
2024-06-12 16:46:33 +0400

    Updated on 2024-06-12 16:46:33 by mhaile

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mhaile <mhaile@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 19:33:00 by mhaile            #+#    #+#             */
-/*   Updated: 2024/06/09 18:49:04 by mhaile           ###   ########.fr       */
+/*   Updated: 2024/06/12 16:45:24 by mhaile           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,16 @@ int main(int ac, char **av) {
 
 		std::cout << "Before: ";
 		sorter.printVec();
+		// sorter.printList();
 
 		sorter.merge_sort_vec();
 		sorter.merge_sort_list();
 		
 		std::cout << "After: ";
-		sorter.printList();
-		// sorter.printVec();
-		std::cout << "Time to process a range of " << ac - 1 << " with std::vector : " << std::fixed << std::setprecision(4) << sorter.getTimeVec() << " ms" << std::endl;
-		std::cout << "Time to process a range of " << ac - 1 << " with std::list : " << std::fixed << std::setprecision(4) << sorter.getTimeList() << " ms" << std::endl;
+		sorter.printVec();
+		// sorter.printList();
+		std::cout << "Time to process a range of " << ac - 1 << " with std::vector : " << std::fixed << std::setprecision(5) << sorter.getTimeVec() << " ms" << std::endl;
+		std::cout << "Time to process a range of " << ac - 1 << " with std::list : " << std::fixed << std::setprecision(5) << sorter.getTimeList() << " ms" << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
